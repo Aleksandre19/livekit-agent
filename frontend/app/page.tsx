818f67be8ea12506070c5ed9440a8117712703d5
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-10-22 23:45:59 +0200

    08. Set up the Prisma and the Next-Auth with the Google Provider.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,5 @@
 import '@/styles/globals.css';
-import { Box, Container, Theme } from '@radix-ui/themes';
+import { Box, Button, Container, Flex, Heading, Text, Theme } from '@radix-ui/themes';
 import VideoBg from './VideoBg';
 
 export default function Page() {
@@ -8,7 +8,24 @@ export default function Page() {
       <Box className="home-container">
         <VideoBg />
         <Container size="3" className="content">
-          Content
+          <Flex align="center" height="100vh">
+            <Flex>
+              <Box>
+                <Heading as="h1" className="my-4">
+                  Talk to Inovation
+                </Heading>
+                <Text as="p">
+                  Try AI assistant and explore its possibility. Experience seamless voice
+                  interactions that feel like natural conversations with a knowledgeable friend. Get
+                  instant responses and creative solutions while speaking in your own comfortable
+                  way.
+                </Text>
+              </Box>
+              <Box>
+                <Button>Get Started</Button>
+              </Box>
+            </Flex>
+          </Flex>
         </Container>
       </Box>
     </Theme>
