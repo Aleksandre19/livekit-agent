ee03ce91e95c58cd3f1ba32194f0c0149226e2e1
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-10-25 01:06:25 +0200

    13. feat(middleware): protect rooms route with auth middleware

---
+++ frontend/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server';
+import { withAuth } from 'next-auth/middleware';
+
+export default withAuth(
+  function middleware(req) {
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+    pages: {
+      signIn: '/',
+    },
+  },
+);
+
+export const config = {
+  matcher: ['/rooms/:path*'],
+};
